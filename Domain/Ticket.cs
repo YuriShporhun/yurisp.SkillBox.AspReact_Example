339de245fedde8e6fb886006e1cd3a7f83745ea2
Юрий <YuriPCLive@live.ru>
2021-09-16 00:02:46 +0300

    Добавил модель билета на самолет

---
+++ Domain/Ticket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Domain
+{
+    /// <summary>
+    /// Информация о билете на самолет
+    /// </summary>
+    public class Ticket
+    {
+        /// <summary>
+        /// Пункт отправления
+        /// </summary>
+        public string From { get; set; }
+
+        /// <summary>
+        /// Пункт назначения
+        /// </summary>
+        public string To { get; set; }
+
+        /// <summary>
+        /// Дата вылета
+        /// </summary>
+        public DateTime Departing { get;set;  }
+
+        /// <summary>
+        /// Дата возврата
+        /// </summary>
+        public DateTime Returning { get; set; }
+
+    }
+}
