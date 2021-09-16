27ad47aa39d66a2f5335fa9da49c56a222c4b1c7
Юрий <YuriPCLive@live.ru>
2021-09-16 03:06:52 +0300

    Обновил контроллер с билетами.

---
+++ API/Controllers/TicketsController.cs
@@ -0,0 +1,35 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketsController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public TicketsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Ticket>>> GetTickets()
+        {
+            return await _context.Tickets.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Ticket>> GetTicket(Guid id)
+        {
+            return await _context.Tickets.FindAsync(id);
+        }
+
+    }
+}
