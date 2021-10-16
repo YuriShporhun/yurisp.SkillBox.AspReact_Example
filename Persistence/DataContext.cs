97afa1e26d12cfc2ea6d9f19dcc642ae04d87e59
Юрий Шпорхун <yuripclive@live.ru>
2021-10-16 19:03:43 +0300

    Заготовка для авторизации и аутентификации.

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,9 +1,11 @@
 ﻿using Domain;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<ApplicationUser>
     {
         public DataContext()
         {
@@ -13,6 +15,11 @@ namespace Persistence
         public DataContext(DbContextOptions options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
 
         public DbSet<Ticket> Tickets { get; set; }
