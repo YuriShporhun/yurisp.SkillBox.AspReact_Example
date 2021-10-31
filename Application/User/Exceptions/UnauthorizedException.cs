66c793e3698ac99f3089c1b0ec58773b23f88402
Юрий Шпорхун <yuripclive@live.ru>
2021-10-31 23:27:42 +0300

    Запрос для логина.

---
+++ Application/User/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.User.Exceptions
+{
+    internal class UnauthorizedException : Exception
+    {
+        internal enum Reason
+        {
+            UserNotFound,
+            WrongPassword
+        }
+        public UnauthorizedException(Reason reason) : base(reason.ToString())
+        {
+            
+        }
+    }
+}
---
+++ Application/User/Queries/Login.cs
@@ -0,0 +1,64 @@
+﻿using Application.User.Exceptions;
+using FluentValidation;
+using Infrastructure.Identity;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.User
+{
+    internal class Login
+    {
+        public class Query : IRequest<ApplicationUser>
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class QueryValidator : AbstractValidator<Query>
+        {
+            public QueryValidator()
+            {
+                RuleFor(x => x.Email).NotEmpty();
+                RuleFor(x => x.Password).NotEmpty();
+            }
+        }
+
+        public class Handler : IRequestHandler<Query, ApplicationUser>
+        {
+            private readonly UserManager<ApplicationUser> _userManager;
+            private readonly SignInManager<ApplicationUser> _signInManager;
+
+            public Handler(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+            {
+                _userManager = userManager;
+                _signInManager = signInManager;
+            }
+
+            public async Task<ApplicationUser> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var user = await _userManager.FindByEmailAsync(request.Email);
+
+                if(user == null)
+                {
+                    throw new UnauthorizedException(UnauthorizedException.Reason.UserNotFound);
+                }
+
+                var result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+
+                if(result.Succeeded)
+                {
+                    return user;
+                }
+
+                throw new UnauthorizedException(UnauthorizedException.Reason.WrongPassword);
+
+            }
+        }
+    }
+}
