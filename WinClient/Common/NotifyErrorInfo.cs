ed3e1d63af54fa1825337f266302bab1bd91fbc2
Юрий Шпорхун <yuripclive@live.ru>
2021-11-02 06:03:51 +0300

    Реализовал NotifyErrorInfo.

--- WinClient/Common/NotifyErrorInfo.cs
+++ WinClient/Common/NotifyErrorInfo.cs
@@ -3,22 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WinClient.Common;
 
 namespace WinClient.NTier.Common
 {
     public class NotifyErrorInfo : Bindable, INotifyDataErrorInfo
     {
-        protected readonly Dictionary<string, List<string>> Errors;
-        public bool HasErrors => throw new NotImplementedException();
+        protected readonly Dictionary<string, List<string>> Errors = new();
+        public bool HasErrors => Errors.Any();
 
         public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
 
         public IEnumerable GetErrors(string propertyName)
         {
-            throw new NotImplementedException();
+            if(propertyName == null && Errors.ContainsKey(propertyName))
+            {
+                return Errors[propertyName];
+            }
+            return Enumerable.Empty<string>();
+        }
+
+        protected void RaiseErrorChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
         }
     }
 }
